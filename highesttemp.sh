df1e3a9593f50a236e3f4e085b161484601b6117
Jonathan2055 <j.mugisha3@alustudent.com>
2025-02-09 23:06:50 +0200

    Correcting Error on Highest Temperature

--- highesttemp.sh
+++ highesttemp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sort -t, -k2 -nr raw_data/satelite_temperature_data.csv | head -10 > analyzed_data/highest_temp.csv
+sort -t, -k3 -nr raw_data/satelite_temperature_data.csv | head -10 > analyzed_data/highest_temp.csv
